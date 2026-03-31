eb7512c7aed4cdd94da9bb8b12783c7599d4f313
FerdNanda <fenandalaia@outlook.com>
2026-03-31 20:44:47 -0300

    adc busca por ID (GET) e cadastro de novo incidente (POST)

--- server.js
+++ server.js
@@ -38,3 +38,39 @@ app.get("/incidentes", async (req, res) => {
 
   res.json(listaIncidentes); //resposta da requisição: retorna os incidentes em formato JSON
 });
+
+
+
+//ROTA ESPECIFICA
+app.get("/incidentes/:id", async (req, res) => {
+
+
+
+
+
+  
+  const db = await criarBanco(); //chama a função para criar a conexão com o banco de dados
+  const { id } = req.params; //extrai o ID do parâmetro da URL
+
+ 
+  const incidenteEspecifico = await db.all("SELECT * FROM incidentes WHERE id = ?", [id]); //consulta SQL para selecionar o incidente pelo ID
+
+
+  res.json(incidenteEspecifico); //resposta da requisição: retorna o incidente em formato JSON
+});
+
+
+//ROTA PARA NOVO INCIDENTE
+
+app.post("/incidentes", async (req, res) => {
+
+  const {tipo_problema,  localizacao,  descricao,   prioridade,  nome_solicitante, data_registro, hora_registro } = req.body; 
+
+  const db = await criarBanco(); //chama a função para criar a conexão com o banco de dados
+
+
+  await db.run("INSERT INTO incidentes (tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)", [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro]); 
+
+
+  res.send(`Incidente registrado: ${tipo_problema} registrado em ${data_registro} por ${nome_solicitante}`)
+});
